55696935c6f99261fdcc5815dabe1a50dae9684e
AlvaroRGZ <alvarorgten@gmail.com>
2025-01-27 21:34:27 +0000

    Added cors config

--- src/main/java/com/hyperspacegroup/fomalhaut/core/tests/LogView.java
+++ src/main/java/com/hyperspacegroup/fomalhaut/core/tests/LogView.java
@@ -1,7 +1,6 @@
 package com.hyperspacegroup.fomalhaut.core.tests;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
